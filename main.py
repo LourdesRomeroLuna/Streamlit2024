ec9cc38c09dd54b2637c01475128358c4c3d0cb9
LourdesRomeroLuna <158647312+LourdesRomeroLuna@users.noreply.github.com>
2024-02-02 20:49:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.title('Streamlit2024')
-n = st.slider("n", 5,100, step=1)
-chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
+st.title('UPC Data Science 2024')
+st.header('Simulador Ventas')
+n = st.slider("cant. ventas", 5,100, step=1)
+chart_data = pd.DataFrame(np.random.randn(n),columns=['ventas'])
 st.line_chart(chart_data)
